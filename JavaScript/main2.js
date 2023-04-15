514cb2eed3ead8c8594cec69c09772a75de2c31e
Clément Corbière <corbiere.clement@hotmail.fr>
2023-04-15 18:25:16 +0200

    Jira11: creation du calendrier

--- JavaScript/main2.js
+++ JavaScript/main2.js
@@ -1,3 +1,5 @@
+// Gestion du changement des onglets *******************************
+
 let dateBtn = document.getElementById("dateBtn");
 let heureBtn = document.getElementById("heureBtn");
 let personneBtn = document.getElementById("personneBtn");
@@ -34,3 +36,131 @@ personneBtn.addEventListener("click", () => {
   btnA.classList.remove("active");
   btnB.classList.remove("active");
 });
+
+// Gestion du calendrier ************************************
+
+let cases = document.getElementsByClassName("case");
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth() + 1;
+let day = date.getDate();
+
+const monthName = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
+
+// action pour avancer ou reculer dans les mois via les flèches
+const UP_MONTH = "upMonth";
+const DOWN_MONTH = "downMonth";
+
+function CALENDRIER_REDUCER(action) {
+  switch (action) {
+    case UP_MONTH:
+      if (month < 12) month++;
+      else {
+        year++;
+        month = 1;
+      }
+      break;
+
+    case DOWN_MONTH:
+      if (month > 0) month--;
+      else {
+        year--;
+        month = 12;
+      }
+      break;
+    default:
+      break;
+  }
+  calendrier(year, month);
+}
+
+document.getElementById("avant").onclick = function () {
+  CALENDRIER_REDUCER(DOWN_MONTH);
+  console.log(month);
+};
+
+document.getElementById("apres").onclick = function () {
+  CALENDRIER_REDUCER(UP_MONTH);
+  console.log(month);
+};
+
+function calendrier(year, month) {
+  const monthNb = month + 12 * (year - 2020);
+
+  let cld = [{ dayStart: 2, length: 31, year: 2020, month: "janvier" }];
+  for (let i = 0; i < monthNb - 1; i++) {
+    let yearSimulé = 2020 + Math.floor(i / 12);
+    const monthsSimuléLongeur = [
+      31,
+      getFévrierLenght(yearSimulé),
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+    let monthsSimuléIndex = i + 1 - (yearSimulé - 2020) * 12;
+
+    cld[i + 1] = {
+      dayStart:
+        (cld[i].dayStart + monthsSimuléLongeur[monthsSimuléIndex - 1]) % 7,
+      length: monthsSimuléLongeur[monthsSimuléIndex],
+      year: 2020 + Math.floor((i + 1) / 12),
+      month: monthName[monthsSimuléIndex],
+    };
+
+    if (cld[i + 1].month === undefined) {
+      cld[i + 1].month = "Janvier";
+      cld[i + 1].length = 31;
+    }
+  }
+  for (let i = 0; i < cases.length; i++) {
+    cases[i].innerText = "";
+  }
+  for (let i = 0; i < cld[cld.length - 1].length; i++) {
+    cases[i + cld[cld.length - 1].dayStart].innerText = i + 1;
+  }
+  document.getElementById("cldT").innerText =
+    cld[cld.length - 1].month.toLocaleUpperCase() +
+    " " +
+    cld[cld.length - 1].year;
+}
+
+calendrier(year, month);
+
+// fonction pour l'année bissextile
+function getFévrierLenght(year) {
+  if (year % 4 === 0) return 29;
+  else return 28;
+}
+
+let cldBoite = document.getElementById("cldBoite");
+
+cldBoite.addEventListener("click", function () {
+  heurePage.style.visibility = "visible";
+  datePage.style.visibility = "hidden";
+  personnePage.style.visibility = "hidden";
+  btnB.classList.add("active");
+  btnA.classList.remove("active");
+  btnC.classList.remove("active");
+});
+
+// Gestion de la selection des heures **********************************
