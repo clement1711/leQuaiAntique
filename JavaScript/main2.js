9957ea6fab552e733f814f7f24329c4d9097bd86
Clément Corbière <corbiere.clement@hotmail.fr>
2023-04-19 22:39:48 +0200

    Jira12: ajout du modal alergie

--- JavaScript/main2.js
+++ JavaScript/main2.js
@@ -164,3 +164,116 @@ cldBoite.addEventListener("click", function () {
 });
 
 // Gestion de la selection des heures **********************************
+
+// Ajout des Adultes
+
+let nbrMan = 2;
+let downA = document.getElementById("downA");
+let upA = document.getElementById("upA");
+let div = `
+<div class="col-2 pastille d-flex pastMan mx-2 my-3">Adulte</div>`;
+let test = document.getElementById("parent");
+let nombreA = document.getElementById("nombreA");
+let nombre = (nombreA.textContent = nbrMan);
+
+const addA = () => {
+  nbrMan++;
+  let targetDiv = document.getElementById("parent");
+  targetDiv.innerHTML += div;
+  nombreA.textContent = nbrMan;
+};
+
+const delA = () => {
+  nbrMan--;
+  let targetDiv = document.getElementById("parent");
+  nombreA.textContent = nbrMan;
+};
+
+upA.addEventListener("click", addA);
+downA.addEventListener("click", delA);
+
+// Ajout des Enfants
+
+let nbrChil = 0;
+let nombreE = document.getElementById("nombreE");
+let upE = document.getElementById("upE");
+let downE = document.getElementById("downE");
+let nombre2 = (nombreE.textContent = nbrChil);
+let div2 = `
+<div class="col-2 pastille d-flex pastChild mx-2 my-3">Enfant</div>`;
+
+const addE = () => {
+  nbrChil++;
+  nombreE.textContent = nbrChil;
+  let targetDiv = document.getElementById("parent");
+  targetDiv.innerHTML += div2;
+};
+
+const delE = () => {
+  nbrChil--;
+  nombreE.textContent = nbrChil;
+  let targetDiv = document.getElementById("parent");
+  targetDiv.innerHTML.removeChild("div2");
+};
+
+upE.addEventListener("click", addE);
+downE.addEventListener("click", delE);
+
+// Places restantes
+
+let nbrPlaceTotal = 15 - (nbrMan + nbrChil);
+
+let nbrPlace = document.getElementById("nbrPlace");
+nbrPlace.innerHTML = nbrPlaceTotal;
+
+if (nbrChil == 0) {
+  downE.style.visibility = "hidden";
+}
+if (nbrMan == 0) {
+  downA.style.visibility = "hidden";
+}
+
+upA.addEventListener("click", () => {
+  nbrPlace.innerHTML--;
+  if (nbrPlace.innerHTML == 0) {
+    upA.style.visibility = "hidden";
+    upE.style.visibility = "hidden";
+  }
+  if (nbrMan > 0) {
+    downA.style.visibility = "visible";
+  }
+});
+
+upE.addEventListener("click", () => {
+  nbrPlace.innerHTML--;
+  if (nbrPlace.innerHTML == 0) {
+    upA.style.visibility = "hidden";
+    upE.style.visibility = "hidden";
+  }
+  if (nbrChil > 0) {
+    downE.style.visibility = "visible";
+  }
+});
+
+downA.addEventListener("click", () => {
+  nbrPlace.innerHTML++;
+  if (nbrMan == 0) {
+    downA.style.visibility = "hidden";
+  }
+  if (nbrPlace.innerHTML > 0) {
+    upA.style.visibility = "visible";
+    upE.style.visibility = "visible";
+  }
+});
+
+downE.addEventListener("click", () => {
+  nbrPlace.innerHTML++;
+  if (nbrChil == 0) {
+    downE.style.visibility = "hidden";
+  }
+
+  if (nbrPlace.innerHTML > 0) {
+    upA.style.visibility = "visible";
+    upE.style.visibility = "visible";
+  }
+});
